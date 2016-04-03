b4ddb95250910eeecb8935e17188d00eaa89f697
slightair <arksutite@gmail.com>
2016-04-03 20:59:25 +0900

    Create project

---
+++ GarageClientSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GarageClientSwift
+//
+//  Created by slightair on 4/3/16.
+//  Copyright © 2016 slightair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
